51fd8c6a853f9503b0594466d7a8de4260f2fba0
Brandon Stewart <113845525+sbrandon96@users.noreply.github.com>
2024-08-15 20:22:09 -0400

    Basic CRUD working
    Got the basic Get/Post working for the add/delete/update functions. Moving on to Heroku hosting step

--- app.py
+++ app.py
@@ -29,10 +29,38 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', set_tasks=tasks)
+    
 
-def test_connection(self):
-    with app.app_context():
-        db.create_all()
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task)
+
+# def test_connection(self):
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
